a5b8ae668c4fe95dd33ef15e9d17461870f21db4
ziadhany <ziadhany2016@gmail.com>
2023-12-09 20:19:19 +0200

    Fix image path
    Signed-off-by: ziadhany <ziadhany2016@gmail.com>

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,60 +1,62 @@
 import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Books} from "../book/book.component";
+import {FormsModule} from "@angular/forms";
 
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
 })
 export class BookListComponent implements OnInit  {
+  searchtext: any;
   bookList: Books[] = [
     {
       id: 1,
       title: 'Harry Potter and The Deathly Hallows',
       description: '',
-      imageUrl: 'assets/book1.jpg'
+      imageUrl: 'assets/images/book1.jpg'
     },
     {
       id: 2,
       title: 'Simple Way Of Piece Life',
       description: '',
-      imageUrl: 'assets/book2.jpg'
+      imageUrl: 'assets/images/book2.jpg'
     },
     {
       id: 3,
       title: 'The Fault In Our Stars',
       description: '',
-      imageUrl: 'assets/book3.jpg'
+      imageUrl: 'assets/images/book3.jpg'
     },
     {
       id: 4,
       title: 'Madelene Brent "A Heritage of Shadows"',
       description: '',
-      imageUrl: 'assets/book4.jpg'
+      imageUrl: 'assets/images/book4.jpg'
     },
     {
       id: 5,
       title: 'The Girl in the letter',
       description: '',
-      imageUrl: 'assets/book5.jpg'
+      imageUrl: 'assets/images/book5.jpg'
     },
     {
       id: 6,
       title: 'Atomic Habits',
       description: '',
-      imageUrl: 'assets/book6.jpg'
+      imageUrl: 'assets/images/book6.jpg'
     }];
   constructor() {}
   ngOnInit(): void {}
 
-  // searchBooks(){
-  //   this.bookList = this.bookList.filter(res=>{
-  //     return res.title.toLocaleLowerCase().match(this.searchtext.toLocaleLowerCase());
-  //   })
-  // }
+  searchBooks(){
+    this.bookList = this.bookList.filter(res=>{
+      return res.title.toLocaleLowerCase().match(this.searchtext.toLocaleLowerCase());
+    })
+  }
 
 }
